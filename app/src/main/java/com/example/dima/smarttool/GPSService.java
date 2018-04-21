decaebf3f1dbbf838cdf2d234319916bb311813c
@gmail.co <dima200214@gmail.com>
2018-04-21 14:25:07 +0500

    notification notifications added

--- app/src/main/java/com/example/dima/smarttool/GPSService.java
+++ app/src/main/java/com/example/dima/smarttool/GPSService.java
@@ -1,6 +1,9 @@
 package com.example.dima.smarttool;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
@@ -9,11 +12,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.AudioManager;
+import android.media.RingtoneManager;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.example.dima.smarttool.DB.NoteHelper;
 import com.example.dima.smarttool.DB.StateHelper;
 import com.google.android.gms.maps.model.LatLng;
 
@@ -26,24 +34,50 @@ public class GPSService extends Service {
 
     static HashMap<Integer, State> stateGpsMap = new HashMap<>();
     static ArrayList<LatLng> stateGpsList = new ArrayList<LatLng>();
+    static HashMap<Integer, Note> noteGpsMap = new HashMap<>();
+    static ArrayList<LatLng> noteGpsList = new ArrayList<LatLng>();
     static final long MIN_INTERVAL = 60 * 1000;                               // время обновления
     private static final String TAG = "mygps";
     BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
     WifiManager wifiManager;
     static AudioManager audioManager;
-
+    private static final String NOTIFICATION_CHANNEL_ID = "note_notification_channel";
+    static  NotificationManager notificationManager;
     @SuppressLint("MissingPermission")
     @Override
     public void onCreate() {
+
+       notificationManager = (NotificationManager) getApplicationContext()
+                .getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    "Notifications", NotificationManager.IMPORTANCE_HIGH);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500,
+                    1000});
+
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
 
 
         LocationManager locationManager =
                 (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_INTERVAL, 100, locationListener);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_INTERVAL, 100, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_INTERVAL,
+                100, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_INTERVAL,
+                100, locationListener);
         StateHelper sh = new StateHelper(getApplicationContext());
         loadStates(sh.getAll());
+        NoteHelper nh = new NoteHelper(getApplicationContext());
+        loadNotes(nh.getAll());
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
@@ -81,8 +115,32 @@ public class GPSService extends Service {
             Log.d(TAG, "долгота:" + location.getLongitude());
             double lat = location.getLatitude();
             double lng = location.getLongitude();
-            if (stateGpsMap.get(checkLatLng(lat, lng)) != null)
-                setState(stateGpsMap.get(checkLatLng(lat, lng)));
+
+
+            if (stateGpsMap.get(checkLatLngState(lat, lng)) != null)
+                setState(stateGpsMap.get(checkLatLngState(lat, lng)));
+            if (noteGpsMap.get(checkLatLngNote(lat, lng)) != null){
+                Note note = noteGpsMap.get(checkLatLngNote(lat, lng));
+
+                Intent notifyIntent = new Intent(getApplicationContext(), ShowNoteActivity.class);
+                notifyIntent.putExtra("name", note.getName());
+                notifyIntent.putExtra("note", note.getText());
+                notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                PendingIntent notifyPendingIntent = PendingIntent.getActivity(
+                        getApplicationContext(), 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_CHANNEL_ID)
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                        .setSmallIcon(R.drawable.note)
+                        .setContentTitle("Note time")
+                        .setContentText("Note: " + note.getName())
+                        .setAutoCancel(true)
+                        .setContentIntent(notifyPendingIntent);
+                notificationManager.notify(note.getId(), builder.build());
+                Toast.makeText(getApplicationContext(), "setNote: "+note.getName(),Toast.LENGTH_SHORT).show();
+            }
 
 
         }
@@ -112,6 +170,17 @@ public class GPSService extends Service {
 
     }
 
+    public void loadNotes(ArrayList<Note> notes) {
+        for (int i = 0; i < notes.size(); i++) {
+            Note note = notes.get(i);
+            LatLng latLng = new LatLng(note.getLat(), note.getLng());
+            noteGpsList.add(latLng);
+            noteGpsMap.put(i, note);
+            Log.d(TAG, "loadNote: " + note.getName());
+        }
+
+    }
+
     private void setState(State state) {
         wifiManager.setWifiEnabled(state.isWiFiState());
         if (state.isBluetoothState()) btAdapter.enable();
@@ -130,7 +199,7 @@ public class GPSService extends Service {
         return (int) (max * in);
     }
 
-    private int checkLatLng(double lat, double lng) {
+    private int checkLatLngState(double lat, double lng) {
 
         for (int i = 0; i < stateGpsList.size(); i++) {
             if (Math.pow((stateGpsList.get(i).latitude - lat), 2) + Math.pow((stateGpsList.get(i).longitude - lng), 2) <= Math.pow(4.986137129513397E-4, 2)) {
@@ -140,6 +209,22 @@ public class GPSService extends Service {
             Log.d(TAG, Math.pow((lat - stateGpsList.get(i).latitude), 2) + Math.pow((lng - stateGpsList.get(i).latitude), 2) + "    " + Math.pow(4.986137129513397E-4, 2));
             Log.d(TAG, stateGpsList.get(i).latitude + "");
             Log.d(TAG, stateGpsList.get(i).longitude + "");
+
+        }
+
+        return 999;
+    }
+
+    private int checkLatLngNote(double lat, double lng) {
+
+        for (int i = 0; i < noteGpsList.size(); i++) {
+            if (Math.pow((noteGpsList.get(i).latitude - lat), 2) + Math.pow((noteGpsList.get(i).longitude - lng), 2) <= Math.pow(4.986137129513397E-4, 2)) {
+                Log.d(TAG, "set " + i + ": " + noteGpsMap.get(i).getName());
+                return i;
+            }
+            Log.d(TAG, Math.pow((lat - noteGpsList.get(i).latitude), 2) + Math.pow((lng - noteGpsList.get(i).latitude), 2) + "    " + Math.pow(4.986137129513397E-4, 2));
+            Log.d(TAG, noteGpsList.get(i).latitude + "");
+            Log.d(TAG, noteGpsList.get(i).longitude + "");
 
         }
 
--- app/src/main/java/com/example/dima/smarttool/NoteAlarmReceiver.java
+++ app/src/main/java/com/example/dima/smarttool/NoteAlarmReceiver.java
@@ -2,6 +2,7 @@ package com.example.dima.smarttool;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +10,6 @@ import android.media.RingtoneManager;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.example.dima.smarttool.DB.NoteHelper;
 
@@ -22,12 +22,10 @@ import java.util.Map;
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class NoteAlarmReceiver extends BroadcastReceiver {
+    private static final String NOTIFICATION_CHANNEL_ID = "note_notification_channel";
     static Map<String, Note> noteTimeMap = new HashMap<String, Note>();                       //зраниение значиний времени старта и номера состояния
     String name;
     Note note;
-    private static final int NOTIFICATION_ID = 1258;
-    private static final String NOTIFICATION_CHANNEL_ID = "note_notification_channel";
-
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -36,17 +34,21 @@ public class NoteAlarmReceiver extends BroadcastReceiver {
         Log.d("alarm", "receiveNote");
         NoteHelper nh = new NoteHelper(context);                                     // инициализация помощника управления состояниямив базе данных
         loadNotes(nh.getAll());
-        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
 
 
-
+        final NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(NOTIFICATION_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notifications", NotificationManager.IMPORTANCE_HIGH);
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    "Notifications", NotificationManager.IMPORTANCE_HIGH);
 
             // Configure the notification channel.
             notificationChannel.setDescription("Channel description");
             notificationChannel.enableLights(true);
             notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500,
+                    1000});
+
             if (notificationManager != null) {
                 notificationManager.createNotificationChannel(notificationChannel);
             }
@@ -54,15 +56,26 @@ public class NoteAlarmReceiver extends BroadcastReceiver {
 
         if (noteTimeMap.get(name) != null) {
             this.note = (noteTimeMap.get(name));
-            Toast.makeText(context, "setNote "+noteTimeMap.get(name), Toast.LENGTH_SHORT);
             Log.d("alarm", "setNote: " + name);
+
+
+            Intent notifyIntent = new Intent(context, ShowNoteActivity.class);
+            notifyIntent.putExtra("name", note.getName());
+            notifyIntent.putExtra("note", note.getText());
+            notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            PendingIntent notifyPendingIntent = PendingIntent.getActivity(
+                    context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+            );
+
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
                     .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                     .setSmallIcon(R.drawable.note)
                     .setContentTitle("Note time")
-                    .setContentText("Note: "+ name);
-            notificationManager.notify(NOTIFICATION_ID, builder.build());
+                    .setContentText("Note: " + note.getName())
+                    .setAutoCancel(true)
+                    .setContentIntent(notifyPendingIntent);
+            notificationManager.notify(note.getId(), builder.build());
         }
 
     }
@@ -84,13 +97,12 @@ public class NoteAlarmReceiver extends BroadcastReceiver {
     }
 
 
-
     private int progInex(float in, int max) {
         in = in / 100;
         return (int) (max * in);
     }
 
-    private void  sendNotification(String name){
+    private void sendNotification(String name) {
 
     }
 
